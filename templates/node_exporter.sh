1a45474c977163717a35a1b109e6747c4010118f
sharonnavon <sharon.nik@gmail.com>
2019-01-22 14:47:25 +0200

    Preparing mid project

--- templates/node_exporter.sh
+++ templates/node_exporter.sh
@@ -5,8 +5,8 @@ sudo apt -qq update
 sudo apt-get install -y python
 
 # Run node exporter app
-git clone https://github.com/ops-school/session-monitoring.git
-nohup python /home/ubuntu/session-monitoring/training_session/my_dummy_exporter.py &
+sudo git clone https://github.com/ops-school/session-monitoring.git /opt/my_dummy_exporter
+nohup python /opt/my_dummy_exporter/training_session/my_dummy_exporter.py &
 
 # Register the app in consul
 cat > /etc/consul.d/python-65433.json << EOF
