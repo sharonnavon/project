9c8ac36a6049737774961516e29f2552dc4f08d9
sharonnavon <sharon.nik@gmail.com>
2019-01-22 10:40:10 +0200

    Preparing mid project

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-//output "consul_servers_public_IP" {
-//  value = ["${aws_instance.consul_server.*.public_ip}"]
-//}
-//
-//output "consul_clients_public_IP" {
-//  value = ["${aws_instance.consul_client.*.public_ip}"]
-//}
-//
+output "consul_servers_public_IP" {
+  value = ["${aws_instance.consul_server.*.public_ip}"]
+}
+
+output "consul_clients_public_IP" {
+  value = ["${aws_instance.consul_client.*.public_ip}"]
+}
+
 
 output "Consul" {
   value = "http://${aws_instance.consul_server.*.public_ip[0]}:8500"
