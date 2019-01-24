6a88edab44a656e95074bf3ed7d82b97a5a558a1
sharonnavon <sharon.nik@gmail.com>
2019-01-24 15:32:48 +0200

    Preparing mid project

--- templates/dummy_app.sh
+++ templates/dummy_app.sh
@@ -2,10 +2,14 @@
 
 # Install Docker-ce
 sudo sed -i 's/us-east-1.ec2.//g' /etc/apt/sources.list
-sudo apt -qq update
-sudo apt install -yqq apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt -qq update
-sudo apt install -yqq docker-ce
+sudo apt -qq update &> /dev/null
+sudo apt install -yqq apt-transport-https ca-certificates curl gnupg2 software-properties-common docker-ce &> /dev/null
 
+# Build & run the dummy container
+sudo mkdir /opt/docker
+cd /opt/docker
+
+sudo sudo docker build -t dummyapp .
+sudo docker run --name=dummyapp1 -d -p 65433:65433 dummyapp
