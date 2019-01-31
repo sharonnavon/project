ccd554be78c483c4d24e29a5bc9e3c9a30b89044
sharonnavon <sharon.nik@gmail.com>
2019-01-31 18:30:05 +0200

    Preparing mid project

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,20 +1,21 @@
-//output "consul_servers_public_IP" {
-//  value = ["${aws_instance.consul_server.*.public_ip}"]
-//}
-//
-//output "consul_clients_public_IP" {
-//  value = ["${aws_instance.consul_client.*.public_ip}"]
-//}
-//
-
 output "Consul" {
   value = "http://${aws_instance.consul_server.*.public_ip[0]}:8500"
 }
-
 output "Grafana" {
   value = "http://${aws_instance.grafana.public_ip}:3000"
 }
-
 output "Prometheus" {
-  value = "curl \"${aws_instance.prometheus.public_ip}:9090\""
+  value = "curl http://${aws_instance.prometheus.public_ip}:9090"
+}
+output "ElasticSearch" {
+  value = "curl http://${aws_instance.elk.public_ip}:9200"
+}
+output "Kibana" {
+  value = "curl http://${aws_instance.elk.public_ip}:5601/app/infra#/logs"
+}
+output "DummyExporterService1" {
+  value = "curl http://${aws_instance.docker_server.*.public_ip[0]}:65433"
+}
+output "DummyExporterService2" {
+  value = "curl http://${aws_instance.docker_server.*.public_ip[1]}:65433"
 }
