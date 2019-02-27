db9389fe8ca2079bf8afd611ef88d3844b220c8f
sharonnavon <sharon.nik@gmail.com>
2019-02-27 21:59:35 +0200

    Preparing final project

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -11,3 +11,20 @@ resource "aws_instance" "prometheus" {
   }
   user_data = "${file("${path.module}/templates/basic_instance.sh")}"
 }
+
+resource "aws_instance" "consul_server" {
+  count = "${var.consul_servers}"
+  ami           = "${var.ami}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet1.id}"
+  key_name      = "${var.key_name}"
+  iam_instance_profile   = "${aws_iam_instance_profile.consul_auto_join.name}"
+  vpc_security_group_ids = ["${aws_security_group.sg_consul.id}"]
+
+  tags = {
+    Name = "consul-server${count.index+1}"
+    consul_server = "true"
+  }
+
+  user_data = "${file("${path.module}/templates/basic_instance.sh")}"
+}
